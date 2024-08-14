f1f657c1fdebb0a4d148bc2c426912a855f113b5
YannThore <yann.thore@gmail.com>
2024-08-14 17:42:01 +0200

    add top app bar and bottom non fonctionnal

--- app/src/main/java/com/example/learningflagsapp/MainActivity.kt
+++ app/src/main/java/com/example/learningflagsapp/MainActivity.kt
@@ -4,33 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
 import androidx.compose.material3.MaterialTheme
-
 import androidx.compose.material3.Surface
-
 import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose.AppTheme
 import com.example.learningflagsapp.ui.FlagListScreen
 import com.example.learningflagsapp.ui.FlagQuizApp
 import com.example.learningflagsapp.ui.FlagQuizScreen
-import com.example.learningflagsapp.ui.theme.LearningFlagsAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LearningFlagsAppTheme {
-                Surface(
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    FlagQuizApp()
-                }
-            }
+            MainContent()
         }
     }
 }
 
+@Composable
+fun MainContent() {
+    AppTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            FlagQuizApp()
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainContent() {
+    MainContent()
+}
--- app/src/main/java/com/example/learningflagsapp/ui/HomeScreen.kt
+++ app/src/main/java/com/example/learningflagsapp/ui/HomeScreen.kt
@@ -5,24 +5,104 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(playerName: String, onStartQuiz: () -> Unit) {
+
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Account", "Score", "Settings")
+
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
-                title = { Text(text = if (playerName.isNotEmpty()) "Welcome, $playerName!" else "Flag Quiz") }
-            )
+            CenterAlignedTopAppBar(
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text(
+                    "Welcome, $playerName",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+
+            navigationIcon = {
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Localized description"
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "Localized description"
+                    )
+                }
+            },
+            scrollBehavior = scrollBehavior,
+        )
+
+        },
+
+        bottomBar = {
+            NavigationBar {
+
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        icon = {
+                            when (item) {
+                                "Account" -> Icon(Icons.Filled.AccountCircle, contentDescription = item)
+                                "Score" -> Icon(Icons.Filled.Star, contentDescription = item)
+                                "Settings" -> Icon(Icons.Filled.Settings, contentDescription = item)
+                            }
+                        },
+                        label = { Text(item) },
+                        selected = selectedItem == index,
+                        onClick = { selectedItem = index }
+                    )
+                }
+            }
         }
     ) { paddingValues ->
         Column(
@@ -38,4 +118,14 @@ fun HomeScreen(playerName: String, onStartQuiz: () -> Unit) {
             }
         }
     }
+
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen(playerName = "John Doe", onStartQuiz = {})
 }
